4ddb5f65e57d7a3030744a9e48bc2fdaead1fc0e
king <king129@vip.163.com>
2018-05-19 23:04:14 +0800

    完成基本功能

---
+++ client_test.go
@@ -0,0 +1,95 @@
+package apns
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/king129/go-apns/cer"
+	"github.com/king129/go-apns/token"
+)
+
+func TestClient_UsignCer(t *testing.T) {
+
+	p12Path := os.Getenv("P12PATH")
+	p12Passwd := os.Getenv("P12PASSWD")
+
+	cer, err := cert.FromP12File(p12Path, p12Passwd)
+	if err != nil {
+		t.Error(err)
+	}
+
+	client := NewClientWithCer(cer)
+
+	m := &Message{
+		Topic:       "com.taihe.test.moblink",
+		DeviceToken: "ac8a3585166d60abd44867195b8f3edb63f4a2d1b2bb77c896913b7dd68716b8",
+		Payload: &MessagePayload{
+			Aps: &MessageAps{
+				Alert: &MessageAlert{
+					Title: "标题",
+					SubTitle: "子标题",
+					Body:  "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试",
+				},
+				Badeg: 100,
+				Sound: MessageAPNSSoundDefault,
+			},
+		},
+	}
+
+	resp, err := client.Push(m)
+	if err != nil {
+		t.Error(err)
+	}
+	if !resp.Success() {
+		t.Error(resp.Reason)
+	} else {
+		fmt.Println("发送成功 apns-id:", resp.ApnsID)
+	}
+
+}
+
+func TestClient_UsignToken(t *testing.T) {
+
+	authKeyPath := os.Getenv("AUTHKEY")
+
+	authKey, err := token.AuthKeyFromFile(authKeyPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	token := &token.Token{
+		AuthKey: authKey,
+		TeamID:  "xxxx",
+		KeyID:   "xxxx",
+	}
+
+	client := NewClientWithToken(token)
+	
+	m := &Message{
+		Topic:       "com.taihe.test.moblink",
+		DeviceToken: "ac8a3585166d60abd44867195b8f3edb63f4a2d1b2bb77c896913b7dd68716b8",
+		Payload: &MessagePayload{
+			Aps: &MessageAps{
+				Alert: &MessageAlert{
+					Title: "标题",
+					SubTitle: "子标题",
+					Body:  "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试",
+				},
+				Badeg: 100,
+				Sound: MessageAPNSSoundDefault,
+			},
+		},
+	}
+
+	resp, err := client.Push(m)
+	if err != nil {
+		t.Error(err)
+	}
+	if !resp.Success() {
+		t.Error(resp.Reason)
+	} else {
+		fmt.Println("发送成功 apns-id:", resp.ApnsID)
+	}
+}
---
+++ example/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/king129/go-apns"
+	"github.com/king129/go-apns/token"
+	"github.com/king129/go-apns/cer"
+	"fmt"
+)
+
+func main() {
+	
+	// Using p12 certificate
+	UsingCer()
+	// Using authKey
+	UsingAuthKey()
+}
+
+func UsingCer()  {
+	
+	cer, err := cert.FromP12File("xxxx", "xxx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	client := apns.NewClientWithCer(cer)
+	
+	m := &apns.Message{
+		Topic: "me.kinghub.apns.demo",
+		DeviceToken: "xxxxxxxxxxxxxxxxx",
+		Payload: &apns.MessagePayload{
+			Aps: &apns.MessageAps{
+				Alert: &apns.MessageAlert{
+					Title: "test",
+					Body: "xxxxxx",
+				},
+				Badeg: 100,
+				Sound: apns.MessageAPNSSoundDefault,
+			},
+		},
+	}
+	
+	client.Push(m)
+}
+
+func UsingAuthKey()  {
+	
+	authKey, err := token.AuthKeyFromFile("xxxx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	token := &token.Token{
+		AuthKey:authKey,
+		TeamID: "xxxx",
+		KeyID: "xxxx",
+	}
+	
+	client := apns.NewClientWithToken(token)
+	
+	m := &apns.Message{
+		Topic: "me.kinghub.apns.demo",
+		DeviceToken: "xxxxxxxxxxxxxxxxx",
+		Payload: &apns.MessagePayload{
+			Aps: &apns.MessageAps{
+				Alert: &apns.MessageAlert{
+					Title: "test",
+					Body: "xxxxxx",
+				},
+				Badeg: 100,
+				Sound: apns.MessageAPNSSoundDefault,
+			},
+		},
+	}
+	client.Push(m)
+}
